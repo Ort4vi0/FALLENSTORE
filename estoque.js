69a947de28edda98a2b18dd924fb26bc3ea74942
Paraizo <eduardoparize.05@gmail.com>
2025-07-23 08:40:09 -0300

    adicionando menu

--- estoque.js
+++ estoque.js
@@ -1,1 +1,39 @@
-efef
+
+const { clear } = require("console");
+const rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+function exibirMenu() {
+    console.log(
+      "=========MENU=========\n1-Adicionar produto\n2-Listar produtos\n3-Atualizar quantidade de produtos\n4-Deletar lembrete\n5-Verificar produtor com quantidade baixa\n0-Sair do programa"
+    );
+    rl.question("Insira a opção desejada.\n", (opcaoMenu) => {
+      opcaoMenu = parseInt(opcaoMenu, 10);
+      switch (opcaoMenu) {
+        case 1:
+          adicionarProduto();
+          break;
+        case 2:
+          listarProdutos();
+          break;
+        case 3:
+          atulizarProdutos();
+          break;
+        case 4:
+          deletarProduto();
+          break;
+        case 5:
+          console, clear();
+          verificarQNT();
+          break;
+        case 0:
+          process.exit();
+          break;
+        default:
+          console.log("Insira uma opção válida!\n");
+          exibirMenu();
+      }
+    });
+}
