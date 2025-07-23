ce1ca6723528944fb93da3f03276e5faab67d89e
Paraizo <eduardoparize.05@gmail.com>
2025-07-23 09:49:49 -0300

    filtro de quantidade adicionado

--- estoque.js
+++ estoque.js
@@ -1,47 +1,74 @@
 const { clear } = require("console");
 const rl = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-let produtos = [];
+let produtos = []
 
 function exibirMenu() {
-  console.log(
-    "=========MENU=========\n1-Adicionar produto\n2-Listar produtos\n3-Atualizar quantidade de produtos\n4-Deletar produtos\n5-Verificar produtor com quantidade baixa\n0-Sair do programa"
-  );
-  rl.question("Insira a opção desejada.\n", (opcaoMenu) => {
-    opcaoMenu = parseInt(opcaoMenu, 10);
-    switch (opcaoMenu) {
-      case 1:
-        adicionarProduto();
-        break;
-      case 2:
-        listarProdutos();
-        break;
-      case 3:
-        atulizarProdutos();
-        break;
-      case 4:
-        deletarProduto();
-        break;
-      case 5:
-        console, clear();
-        verificarQNT();
-        break;
-      case 0:
-        process.exit();
-        break;
-      default:
-        console.log("Insira uma opção válida!\n");
-        exibirMenu();
+    console.log(
+      "=========MENU=========\n1-Adicionar produto\n2-Listar produtos\n3-Pesquisar produto\n4-Atualizar quantidade de produtos\n5-Deletar produto\n6-Verificar produtor com quantidade baixa\n0-Sair do programa"
+    );
+    rl.question("Insira a opção desejada.\n", (opcaoMenu) => {
+      opcaoMenu = parseInt(opcaoMenu, 10);
+      switch (opcaoMenu) {
+        case 1:
+          adicionarProduto();
+          break;
+        case 2:
+          listarProdutos();
+          break;
+        case 3:
+          pesquisarProdutos();
+          break;
+        case 4:
+          atulizarProdutos();
+          break;
+        case 5:
+          deletarProduto();
+          break;
+        case 6:
+          console.clear();
+          verificarQNT();
+          break;
+        case 0:
+          process.exit();
+          break;
+        default:
+          console.log("Insira uma opção válida!\n");
+          exibirMenu();
+      }
+    });
+}
+
+function deletarProduto(){
+    console.clear()
+    if(produtos.lenght <= 0){
+        console.log('Você não tem produtos para deletar.')
+        console.log('',exibirMenu)
     }
-  });
+    produtos.forEach((produto, index) => {
+        console.log(`ID: ${index + 1} || Produto: ${produto.nome} | Preço: ${produto.preço} | Quantidade: ${produto.quantidade}`)
+    })
+    rl.question('Digite o ID do produto que deseja deletar:\n',(opçãoDeletar) => {
+        opçãoDeletar = parseInt(opçãoDeletar) - 1;
+        if(opçãoDeletar < 0 || opçãoDeletar > produtos.length){
+            console.log('Opção inválida. Retornando ao menu...')
+            exibirMenu()
+        }else{
+            console.clear()
+            produtos.splice(opçãoDeletar, 1)
+            console.log(`Produto ${produtos[opçãoDeletar].nome} deletado!`)
+            exibirMenu()
+        }
+    })
 }
 
 function adicionarProduto() {
   rl.question("Digite o nome do produto: ", (nome) => {
     rl.question("Digite a quantidade: ", (quantidade) => {
+
       if (quantidade < 0) {
         console.clear();
         console.log("Digite uma quantidade valida.");
@@ -62,39 +89,63 @@ function adicionarProduto() {
   });
 }
 
-function verificarQNT() {
-  if (produtos.length == 0) {
-    console.log("Nenhum produto registrado\n");
-    exibirMenu();
+function listarProdutos() {
+  console.clear()
+  if (produtos.length <= 0) {
+    console.log("Não há produtos.")
+    exibirMenu()
   } else {
-    rl.question(
-      "Insira qual o a quantidade que deseja verificar: ",
-      (qntBaixa) => {
-        qntBaixa = parseInt(qntBaixa, 10);
-        if (isNaN(qntBaixa)) {
-          console.clear();
-          console.log("Quantidade inválida, insira novamente!");
-          verificarQNT();
-        } else {
-          console.clear();
-          console.log(
-            `======Produtos com quantidade abaixo de ${qntBaixa}:=======`
-          );
-          produtos.forEach((produto, index) => {
-            if (produto.quantidade < qntBaixa) {
-              console.log(
-                `${index + 1} - Produto: ${produto.nome} | Preço: ${
-                  produto.valor
-                } | Quantidade: ${produto.quantidade}\n`
-              );
-              console.log("============================================");
-            }
-          });
-          exibirMenu();
-        }
-      }
-    );
+    console.log("======PRODUTOS======")
+    produtos.forEach((produto, index) => {
+      console.log(
+        `ID: ${index + 1} | Produto: ${produto.nome}  | Quantidade: ${produto.quantidade} | Valor: R$ ${produto.valor}`
+      )
+    })
+    exibirMenu()
   }
 }
 
-exibirMenu();
+function pesquisarProdutos(){
+  rl.question("Qual o nome do produto que deseja procurar?\n", (filtro) => {
+      const busca = produtos.filter((produtos) => produtos.nome == filtro)
+      console.log(busca)
+      console.log("\nPressione Enter para voltar ao menu")
+      return rl.question("", exibirMenu)
+    })
+}
+
+function verificarQNT() {
+    if (produtos.length == 0) {
+      console.log("Nenhum produto registrado\n");
+      exibirMenu();
+    } else {
+      rl.question(
+        "Insira qual a quantidade que deseja verificar: ",
+        (qntBaixa) => {
+          qntBaixa = parseInt(qntBaixa, 10);
+          if (isNaN(qntBaixa)) {
+            console.clear();
+            console.log("Quantidade inválida, insira novamente!");
+            verificarQNT();
+          } else {
+            console.clear();
+            console.log(
+              `======Produtos com quantidade abaixo de ${qntBaixa}:=======`
+            );
+            produtos.forEach((produto, index) => {
+              if (produto.quantidade < qntBaixa) {
+                console.log(
+                  `${index + 1} - Produto: ${produto.nome} | Preço: ${
+                    produto.valor
+                  } | Quantidade: ${produto.quantidade}\n`
+                );
+                console.log("============================================");
+              }
+            });
+            exibirMenu();
+          }
+        }
+      );
+    }
+  }
+exibirMenu()
